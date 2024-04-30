95ce2acd7827ea6ce972646249d4403308de0bf5
tevynhoem <tevymomnh193@gmail.com>
2024-04-30 14:23:02 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/home', function () {
     return view('frontend.home');
 });
+
+Route::get('/administrator', function () {
+    return view('admin.blank');
+});
